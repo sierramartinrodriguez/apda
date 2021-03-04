688142db5c496de2b63ca63ae83dbf2f52c3542b
fxmrodriguez <rodriguez.f@husky.neu.edu>
2021-03-03 18:58:23 -0600

    stripping

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hi")
+# Using readlines()
+bKeyFile = open('csvFiles/bKey.csv', 'r')
+lines = bKeyFile.readlines()
+ 
+# counter
+count = 0
+
+# Strips the newline character
+for line in lines:
+    count += 1
+    loLine = line.split("=")
+    
+    if len(loLine) != 2:
+        print("Uh oh")
+    
+print(len(lines))
